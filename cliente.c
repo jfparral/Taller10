8bff7e367f2017994c63ed5f1e57df8a396853c8
Jonathan <jparrales15@gmail.com>
2017-07-20 20:09:24 -0500

    inicializando

---
+++ cliente.c
@@ -0,0 +1,97 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFSIZE 1
+#define	ERROR	-1
+//#define	PUERTO	4555
+
+int main(int argc, char *argv[]){
+	struct sockaddr_in stSockAddr;
+	int Res;
+	int SocketFD;
+	char buffer[BUFFSIZE];
+	char mensaje[80];
+	int puerto;
+	FILE *archivo;
+
+	if(argc == 1){
+		printf("Uso: ./cliente <ip> <puerto> <directorio a leer> <nombre del archivo>\n");
+		exit(-1);
+	}
+
+	if(argc != 5){
+		printf( "por favor especificar un numero de puerto\n");
+	}
+
+
+	/*Se abre el archivo a enviar*/
+	archivo = fopen(argv[3],"rb");
+
+	/*Se crea el socket*/
+	SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	/*Se verifica la integridad del archivo*/
+	if(!archivo){
+		perror("Error al abrir el archivo:");
+		exit(EXIT_FAILURE);
+	}
+    
+	/*Se verifica la integridad del socket*/
+	if (SocketFD == ERROR){
+		perror("cannot create socket");
+		exit(EXIT_FAILURE);
+	}
+
+	/*Se configura la dirección del socket del cliente*/
+	memset(&stSockAddr, 0, sizeof stSockAddr);
+	puerto = atoi(argv[2]);
+	stSockAddr.sin_family = AF_INET;
+	stSockAddr.sin_port = htons(puerto);
+	Res = inet_pton(AF_INET, argv[1], &stSockAddr.sin_addr);
+
+	if (0 > Res){
+		perror("error: El primer parametro no es una familia de direcciónes");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}else if (Res == 0){
+		perror("char string (El segundo parametro no contiene una dirección IP válida");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	if (connect(SocketFD, (struct sockaddr *)&stSockAddr, sizeof stSockAddr) == ERROR){
+		perror("Error a la hora de conectarse con el cliente");
+		close(SocketFD);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Se ha conectado con el servidor:%s\n",(char *)inet_ntoa(stSockAddr.sin_addr));
+
+	char ruta[BUFFSIZE]={0};
+	if(send(SocketFD,ruta,BUFFSIZE,0) == ERROR)
+			perror("Error al enviar el ruta:");
+
+	/*Se envia el archivo*/
+	while(!feof(archivo)){
+		fread(buffer,sizeof(char),BUFFSIZE,archivo);
+		if(send(SocketFD,buffer,BUFFSIZE,0) == ERROR)
+			perror("Error al enviar el arvhivo:");
+	}
+	
+	read(SocketFD,mensaje,sizeof(mensaje));
+	printf("\nConfirmación recibida:\n%s\n",mensaje);
+	
+	read(SocketFD,mensaje,sizeof(mensaje));
+	//printf("\nMD5SUM:\n%s\n",mensaje);
+	
+	fclose(archivo);
+	close(SocketFD);
+
+	return 0;
+}//End main
---
+++ servidor_multiproceso.c
@@ -0,0 +1,157 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*Definición de constantes*/
+#define BUFFSIZE 1
+#define ERROR		-1
+
+#ifndef HOST_NAME_MAX 
+#define HOST_NAME_MAX 256 
+#endif
+
+/*Prototipos de función*/
+void recibirArchivo(int SocketFD, FILE *file);
+void enviarConfirmacion(int SocketFD);
+void enviarMD5SUM(int SocketFD);
+
+
+int main(int argc, char *argv[]){
+	struct sockaddr_in stSockAddr;
+    struct sockaddr_in clSockAddr;
+	FILE *archivo;
+	char *host;
+	int SocketServerFD;
+	int SocketClientFD;
+	int clientLen;
+	int puerto;
+	
+
+	/*Verifica que el número de parametros sea el correcto*/
+	if(argc == 1){
+		printf("Uso: ./servidor <ip> <puerto>\n");
+		exit(-1);
+	}
+
+	if(argc != 3){
+		printf( "por favor especificar un numero de puerto\n");
+	}
+
+	/*Se crea el socket*/
+	if((SocketServerFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == ERROR){
+		perror("No se puede crear el socket");
+		exit(EXIT_FAILURE);
+	}//End if-SocketFD
+ 
+	/*Se configura la dirección del socket*/
+	memset(&stSockAddr, 0, sizeof stSockAddr);
+	puerto = atoi(argv[2]);
+	stSockAddr.sin_family = AF_INET;
+	stSockAddr.sin_port = htons(puerto);
+	stSockAddr.sin_addr.s_addr = INADDR_ANY;
+ 
+	if(bind(SocketServerFD,(struct sockaddr *)&stSockAddr, sizeof stSockAddr) == ERROR){
+		perror("Error en bind");
+		close(SocketServerFD);
+		exit(EXIT_FAILURE);
+	}//End if-bind
+	inet_pton(AF_INET, argv[1], &stSockAddr.sin_addr);
+	printf("Socket atado a la dirección %s\n",(char *)inet_ntoa(stSockAddr.sin_addr));
+	
+	//darle el nombre del puerto que esta escuchando
+	if ((host = malloc(HOST_NAME_MAX)) == NULL) 
+		printf(" malloc error"); 
+	gethostname( host, HOST_NAME_MAX);	
+	printf("Nombre del host: %s\n",host);
+
+	if(listen(SocketServerFD, 10) == ERROR){
+		perror("Error listen");
+		close(SocketServerFD);
+		exit(EXIT_FAILURE);
+	}//End if-listen
+
+	while (1){
+		clientLen = sizeof(clSockAddr);
+
+		//Espera por la conexión de un cliente//
+		SocketClientFD = accept(SocketServerFD, 
+						    (struct sockaddr *) &clSockAddr,(socklen_t *)
+						    &clientLen);
+		if (SocketClientFD < 0){
+			perror("Fallo para aceptar la conexión del cliente");
+		}//End if-accept
+
+		/*Se configura la dirección del cliente*/
+		clSockAddr.sin_family = AF_INET;
+		clSockAddr.sin_port = htons(puerto);
+		printf("Cliente conectado: %s\n",inet_ntoa(clSockAddr.sin_addr));
+
+		/*Se recibe la ruta del cliente*/
+        char rutacliente[BUFFSIZE]={0};
+        char ruta[10000000]={0};
+        char imagen[10000000]={0};
+        while((recibido = recv(SocketFD, rutacliente, BUFFSIZE, 0)) == 0){
+		    fwrite(ruta,sizeof(char),1,file);
+	    }//Termina la recepción de la ruta
+        int fd,fd1;
+        fd=open(ruta,O_RDONLY);
+        while(!feof(fd1)){
+            fread(imagen,sizeof(char),10000000,fd);
+            if(send(SocketFD,imagen,10000000,0) == ERROR)
+                perror("Error al enviar el arvhivo:");
+	    }
+		recibirArchivo(SocketClientFD,archivo);
+		
+
+	}//End infinity while
+
+ 	close(SocketClientFD);
+	close(SocketServerFD);
+	return 0;
+}//End main program
+
+void recibirArchivo(int SocketFD, FILE *file){
+	char buffer[BUFFSIZE];
+	int recibido = -1;
+
+	/*Se abre el archivo para escritura*/
+	file = fopen("archivoRecibido","wb");
+	enviarConfirmacion(SocketFD);
+	enviarMD5SUM(SocketFD);
+	while((recibido = recv(SocketFD, buffer, BUFFSIZE, 0)) > 0){
+		//printf("%s",buffer);
+		fwrite(buffer,sizeof(char),1,file);
+	}//Termina la recepción del archivo
+
+	fclose(file);
+	
+
+}//End recibirArchivo procedure
+
+void enviarConfirmacion(int SocketFD){
+	char mensaje[80] = "Paquete Recibido";
+	printf("\nConfirmación enviada\n");
+	if(write(SocketFD,mensaje,sizeof(mensaje)) == ERROR)
+			perror("Error al enviar la confirmación:");
+
+	
+}//End enviarConfirmacion
+
+void enviarMD5SUM(int SocketFD){
+	FILE *tmp;//Apuntador al archivo temporal que guarda el MD5SUM del archivo.
+	char *fileName = "verificacion";
+	char md5sum[80];
+	system("md5sum archivoRecibido >> verificacion");
+	
+	tmp = fopen(fileName,"r");
+	fscanf(tmp,"%s",md5sum);	
+	//printf("\nMD5SUM:%s\n",md5sum);	
+	write(SocketFD,md5sum,sizeof(md5sum));
+	fclose(tmp);
+
+}//End enviarMD5DUM
